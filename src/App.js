eec121f92e24e0fca18b69cae89d1d67e0117018
nikola-m889 <62055062+nikola-m889@users.noreply.github.com>
2021-01-24 18:08:28 +0000

    router fix
    removing 'exact' from home path

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { GlobalState } from "./Context/GlobalState";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [movies, addMovies] = useState([]);
+  const [favorites, addFavorites] = useState([]);
+  const [searchMovie, setSearchMovie] = useState("");
+
+  const findMovie = async (searchMovie) => {
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=731f3d186278f13415d6f4a26e0ff069&query=${searchMovie}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        addMovies(data.results);
+      });
+  };
+
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=731f3d186278f13415d6f4a26e0ff069&page=1"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        addMovies(data.results);
+      });
+  }, []);
+
+  if (searchMovie) {
+    findMovie(searchMovie);
+  }
+
+  useEffect(() => {
+    const StorageData = JSON.parse(localStorage.getItem("movie-data"));
+    if (StorageData) {
+      addFavorites(StorageData);
+    }
+  }, []);
+
+  const saveToStorage = (data) => {
+    localStorage.setItem("movie-data", JSON.stringify(data));
+  };
+
+  const addingFavorites = (result) => {
+    const FavoriteList = [...favorites, result];
+    addFavorites(FavoriteList);
+    saveToStorage(FavoriteList);
+  };
+
+  const deleteFavorites = (result) => {
+    const FavoriteList = favorites.filter((fav) => fav.id !== result.id);
+    addFavorites(FavoriteList);
+    saveToStorage(FavoriteList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalState.Provider
+      value={{
+        movies,
+        addMovies,
+        searchMovie,
+        setSearchMovie,
+        favorites,
+        addFavorites,
+        addingFavorites,
+        deleteFavorites,
+      }}
+    >
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/favorites" component={Favorites} />
+        </Switch>
+      </Router>
+    </GlobalState.Provider>
   );
-}
+};
 
 export default App;
